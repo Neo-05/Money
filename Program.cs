f8222ee11ee418a6af5a5dd79085581f50c1db73
Germ <germainpourignaux@gmail.com>
2023-12-18 15:27:09 +0100

    finition dal béton

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure l’application pour qu’elle crée une nouvelle instance de SqlConnection avec la chaîne de connexion “Default” chaque fois qu’une DbConnection est nécessaire.
+builder.Services.AddTransient<DbConnection>(service =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("Default");
+    return new SqlConnection(connectionString);
+});
 
 var app = builder.Build();
 
